5c3303fac204f6ca94bfd5d1227e9cf50d328e6f
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-04-29 14:57:08 +0000

    exam11_mailer_test7

--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -16,7 +16,7 @@ class NoticeMailer < ApplicationMailer
     @contant = contact
 
     mail to: @contact.email,
-    subject: '【Achieve】お問い合わせが送信されました'
+         subject: '【Achieve】お問い合わせが送信されました'
   end
 
 end
