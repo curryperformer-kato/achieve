6ccbc7a186c5dd77335e319f82946a006346d9c8
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-06-14 01:40:09 +0000

    exam14-2-finished

---
+++ app/models/poem.rb
@@ -0,0 +1,3 @@
+class Poem < ActiveResource::Base
+  self.site = "https://salty-falls-25471.herokuapp.com/"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+  
+  resources :poems, only: [:index, :show]
 
   root 'top#index'
 
