8a65c60c419723fe0935c1944a4abc8b99decbac
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-06-16 09:21:21 +0000

    dive15-finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,19 +12,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  def destroy
-    @comment = current_user.comments.build(comment_params)
-    @blog = @comment.blog
-    respond_to do |format|
-      if @comment.destroy
-        format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
-        format.js { render :index }
-      else
-        format.html { render :new }
-      end
-    end
-  end
-
   private
     def comment_params
       params.require(:comment).permit(:blog_id, :content)
