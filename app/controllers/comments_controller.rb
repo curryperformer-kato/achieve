1ea08b9e0c641784a0b63f200b6de6e16c32d25e
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-06-17 13:43:02 +0000

    dive15-edit-test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,22 +14,31 @@ class CommentsController < ApplicationController
 
   def edit
     @comment = current_user.comments.build(comment_params)
+    @blog = @comment.blog
   end
 
   def update
     @comment = current_user.comments.build(comment_params)
-    @comment.update(comment_params)
-    if @blog.save
-      redirect_to blog_comment_path, notice: "ブログを更新しました！"
-    else
-      render 'new'
-    end
+    @blog = @comment.blog
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを更新しました。' }
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
   end
 
   def destroy
     @comment = current_user.comments.build(comment_params)
-    @comment.destroy
-    redirect_to blog_comment_path, notice: "ブログを削除しました！"
+    @blog = @comment.blog
+    respond_to do |format|
+      if @comment.destroy
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを削除しました。' }
+        format.js { render :index }
+      else
+        format.html { render :new }
+      end
   end
 
   private
