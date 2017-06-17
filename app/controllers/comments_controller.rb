1a71b27fe9cbb91af8c7909beabe46b5563cddec
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-06-17 14:49:25 +0000

    dive15-edit-test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -23,7 +23,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
       format.html { redirect_to blog_path(@blog), notice: 'コメントを更新しました。' }
-      format.js { render :edit }
+      format.js { render :form }
       else
       render 'new'
       end
