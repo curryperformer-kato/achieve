1b56c22573f1198c244585308bd05e03c385cd2d
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-06-28 07:58:42 +0000

    dive19-2-test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,7 +9,7 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
         format.js { render :index }
-        Pusher.trigger('test_channel', 'comment_created', {
+        Pusher.trigger('production_channel', 'comment_created', {
           message: 'あなたの作成したブログにコメントが付きました'
         })
       else
