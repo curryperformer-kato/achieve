8635375e95089ec8a1d08b4e1e19614d798c852a
Yasuaki Kato <charly.clearlingcoach@gmail.com>
2017-07-10 05:53:13 +0000

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,7 +45,7 @@ namespace :deploy do
 
   desc 'Run seed'
   task :seed do
-    on roles(:app) do
+    on roles(:achieve) do
       with rails_env: fetch(:rails_env) do
         within current_path do
           execute :bundle, :exec, :rake, 'db:seed'
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '52.193.88.163', user: 'app', roles: %w{achieve db web}
+server '52.193.88.163', user: 'achieve', roles: %w{achieve db web}
 set :ssh_options, keys: '/home/ubuntu/.ssh/aws_rsa'
